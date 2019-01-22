1a0cef7992cc4df163457a12033398b947fc69a9
jennifer <jlesteshanby@icloud.com>
2019-01-22 11:47:18 -0500

    Application java and test

--- src/main/java/virtualpet/Application.java
+++ src/main/java/virtualpet/Application.java
@@ -1,11 +1,24 @@
 package virtualpet;
 
+import java.util.Scanner;
+
 public class Application {
 
 	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
 		
+String commands = "Commands\n Adopt - adopt a pet\n Organic - choosing an animal pet\n Robotic - choosing a robot pet\n Feed <petname> - feeds your pet\n Play <petname> - plays with your pet\n Vet <petname> - Takes your pet to the vet to improve health\n Rest <petname> - Allows your pet to rest, and you can play with a new pet\n Shelter <petname> - Sends your current pet back to the shelter.  THIS WILL BE PERMANENT.\n Clean <petname> - Cleans your pet and its space\n Exit - Exits VPet";
+	// System.out.println(commands);
+	System.out.println("Type start to start. Type \"help\" for list of commands.");		
+	String initPrompt = input.nextLine();	
+	
+	if (initPrompt.toLowerCase().equals("help")) {
+		System.out.println(commands);
+	} else if (initPrompt.toLowerCase().equals("start")){
 		
-
+	} else {
+		System.out.println("You didn't type a proper command");
 	}
+		}
 
 }
---
+++ src/test/java/virtualpet/ApplicationTest.java
@@ -0,0 +1,10 @@
+package virtualpet;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+@Test
+public void shouldReturnCommands() {
+	String commands = "commands";
+}
+}
