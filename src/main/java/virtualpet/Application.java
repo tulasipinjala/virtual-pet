447af5524aed24d6299188cb04bf6f15704d516d
Anthony Lam <lamah314@gmail.com>
2019-01-22 15:39:54 -0500

    app name code

--- src/main/java/virtualpet/Application.java
+++ src/main/java/virtualpet/Application.java
@@ -5,20 +5,21 @@ import java.util.Scanner;
 public class Application {
 
 	public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
-		
-String commands = "Commands\n Adopt - adopt a pet\n Organic - choosing an animal pet\n Robotic - choosing a robot pet\n Feed <petname> - feeds your pet\n Play <petname> - plays with your pet\n Vet <petname> - Takes your pet to the vet to improve health\n Rest <petname> - Allows your pet to rest, and you can play with a new pet\n Shelter <petname> - Sends your current pet back to the shelter.  THIS WILL BE PERMANENT.\n Clean <petname> - Cleans your pet and its space\n Exit - Exits VPet";
-	// System.out.println(commands);
-	System.out.println("Type start to start. Type \"help\" for list of commands.");		
-	String initPrompt = input.nextLine();	
-	
-	if (initPrompt.toLowerCase().equals("help")) {
-		System.out.println(commands);
-	} else if (initPrompt.toLowerCase().equals("start")){
-		
-	} else {
-		System.out.println("You didn't type a proper command");
-	}
+		Scanner input = new Scanner(System.in);
+
+		String commands = "Commands\n Adopt - adopt a pet\n Organic - choosing an animal pet\n Robotic - choosing a robot pet\n Feed <petname> - feeds your pet\n Play <petname> - plays with your pet\n Vet <petname> - Takes your pet to the vet to improve health\n Rest <petname> - Allows your pet to rest, and you can play with a new pet\n Shelter <petname> - Sends your current pet back to the shelter.  THIS WILL BE PERMANENT.\n Clean <petname> - Cleans your pet and its space\n Exit - Exits VPet";
+		// System.out.println(commands);
+		System.out.println("Please type in your first pet's name. Type \"help\" for list of commands.");
+		String initPrompt = input.nextLine();
+
+		if (initPrompt.toLowerCase().equals("help")) {
+			System.out.println(commands);
+			System.out.println("Please type in your pet's name");
+			initPrompt = input.nextLine();
 		}
+ 
+		VirtualPet pet1 = new VirtualPet(initPrompt);
+		System.out.println("Your pet's name is " + pet1.getName());
+	}
 
 }
