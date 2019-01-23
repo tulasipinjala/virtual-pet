60dd3dd17ff93cadb28ebe1b13b57024bcee9ead
jennifer <jlesteshanby@icloud.com>
2019-01-23 11:53:43 -0500

    added cage cleanliness in virtual pet shelter

---
+++ src/main/java/virtualpet/VirtualPetShelter.java
@@ -0,0 +1,17 @@
+package virtualpet;
+
+public class VirtualPetShelter {
+
+	private int cageCleanliness = 100;
+	
+	//Constructor
+	public VirtualPetShelter() {
+		
+	}
+	
+//Accessor method
+	public int getCageCleanliness() {
+		
+		return cageCleanliness;
+	}
+}
