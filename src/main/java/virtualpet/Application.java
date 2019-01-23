ec72ec0262efbca5b9d358acfd50e9a506b81141
jennifer <jlesteshanby@icloud.com>
2019-01-23 10:54:16 -0500

    remove application test, update application.

--- src/main/java/virtualpet/Application.java
+++ src/main/java/virtualpet/Application.java
@@ -6,9 +6,18 @@ public class Application {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-
-		String commands = "Commands\n Adopt - adopt a pet\n Organic - choosing an animal pet\n Robotic - choosing a robot pet\n Feed <petname> - feeds your pet\n Play <petname> - plays with your pet\n Vet <petname> - Takes your pet to the vet to improve health\n Rest <petname> - Allows your pet to rest, and you can play with a new pet\n Shelter <petname> - Sends your current pet back to the shelter.  THIS WILL BE PERMANENT.\n Clean <petname> - Cleans your pet and its space\n Exit - Exits VPet";
-		// System.out.println(commands);
+// This section determines pet;
+//chooses a pet name; allows user to choose help to find a list of commands
+		String commands = "Commands\n Adopt - adopt a pet\n"
+				+ " Organic - choosing an animal pet\n"
+				+ " Robotic - choosing a robot pet\n"
+				+ " Feed <petname> - feeds your pet\n"
+				+ " Play <petname> - plays with your pet\n"
+				+ " Vet <petname> - Takes your pet to the vet to improve health\n"
+				+ " Rest <petname> - Allows your pet to rest, and you can play with a new pet\n"
+				+ " Shelter <petname> - Sends your current pet back to the shelter.  THIS WILL BE PERMANENT.\n"
+				+ " Clean <petname> - Cleans your pet and its space\n"
+				+ " Exit - Exits VPet";
 		System.out.println("Please type in your first pet's name. Type \"help\" for list of commands.");
 		String initPrompt = input.nextLine();
 
