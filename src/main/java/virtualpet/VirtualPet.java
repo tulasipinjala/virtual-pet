9aae0f56055e5e2133eb6b5333cc7d90396580c9
Anthony Lam <lamah314@gmail.com>
2019-01-25 10:42:44 -0500

    adjusted virtualpet

--- src/main/java/virtualpet/VirtualPet.java
+++ src/main/java/virtualpet/VirtualPet.java
@@ -7,9 +7,9 @@ public class VirtualPet {
 	private int boredom = 100;
 	private int health = 100;
 	private int dirty = 100;
-	
+
 	private boolean organic = true;
-	
+
 	private int prevHunger = 100;
 	private int prevBoredom = 100;
 	private int prevHealth = 100;
@@ -24,8 +24,7 @@ public class VirtualPet {
 	}
 
 	// Method
-	public void tick (int time) {
-		updatePrevProperties();
+	public void tick(int time) {
 		hunger -= time;
 		boredom -= time;
 		health -= time;
@@ -34,31 +33,31 @@ public class VirtualPet {
 			System.exit(0);
 		}
 	}
+
 	public void feed() { // Increase distance from 0 hunger
-		updatePrevProperties();
 		hunger += 50;
 	}
+
 	public void play() { // Must play with your pet
-		updatePrevProperties();
 		boredom += 50;
 	}
+
 	public void checkup() { // Increase distance from 0 hunger
-		updatePrevProperties();
 		health += 50;
 	}
+
 	public void clean() { // Increase distance from 0 dirty
-		updatePrevProperties();
 		dirty += 50;
 	}
-	
-	private void updatePrevProperties() {
+
+	public void updatePrevProperties() {
 		prevHunger = hunger;
 		prevBoredom = boredom;
 		prevHealth = health;
 		prevDirty = dirty;
 	}
-	
-	//Accessor method
+
+	// Accessor method
 	public String getName() {
 		return name;
 	}
@@ -66,6 +65,7 @@ public class VirtualPet {
 	public boolean getOrganic() {
 		return organic;
 	}
+
 	public int getHunger() {
 		return hunger;
 	}
@@ -77,14 +77,14 @@ public class VirtualPet {
 	public int getHealth() {
 		return health;
 	}
+
 	public int getDirty() {
 		return dirty;
 	}
+
 	public void getStatus() {
-		System.out.println(name + "\n Hunger is at " + hunger
-				+ "\n Boredom is at " + boredom
-				+ "\n Health is at " + health
-				+ "\n Dirty is at " + dirty);
+		System.out.println(name + "\n Hunger is at " + hunger + "\n Boredom is at " + boredom + "\n Health is at "
+				+ health + "\n Dirty is at " + dirty);
 	}
 
 //	public void getStatusChange() {
@@ -97,33 +97,26 @@ public class VirtualPet {
 //				+ "\n Dirty \t \t" + prevDirty + "\t" 
 //				+ appropriateArrow(prevDirty, dirty) + "\t"+ dirty);
 //	}
-	
+
 	public void getStatusChange() {
-	System.out.println(name + "\n Hunger \t" + prevHunger + "\t" 
-				+ "=>" + "\t" + hunger
-			+ "\n Boredom \t" + prevHunger + "\t" 
-			+ "=>" + "\t"+ boredom
-			+ "\n Health \t" + prevHealth + "\t" 
-			+ "=>" + "\t"+ health
-			+ "\n Dirty \t \t" + prevDirty + "\t" 
-			+ "=>" + "\t"+ dirty);
-}
-	
-	//special methods
+		System.out.println(name + "\n Hunger \t" + prevHunger + "\t" + "=>" + "\t" + hunger + "\n Boredom \t"
+				+ prevHunger + "\t" + "=>" + "\t" + boredom + "\n Health \t" + prevHealth + "\t" + "=>" + "\t" + health
+				+ "\n Dirty \t \t" + prevDirty + "\t" + "=>" + "\t" + dirty);
+	}
+
+	// special methods
 	private static String printCapitalizedVersion(String message) {
-		return message.substring(0,1).toUpperCase() + message.substring(1).toLowerCase();
+		return message.substring(0, 1).toUpperCase() + message.substring(1).toLowerCase();
 	}
-	
-	private String appropriateArrow(int int1, int int2) {
-		if (int1 > int2) {
-			return "\u2193";
-		} else if (int1 < int2){
-			return "\u2191";
-		} else {
-			return "=";
-		}
-	}
-		
+
+//	private String appropriateArrow(int int1, int int2) {
+//		if (int1 > int2) {
+//			return "\u2193";
+//		} else if (int1 < int2){
+//			return "\u2191";
+//		} else {
+//			return "=";
+//		}
+//	}
+
 }
-
-
