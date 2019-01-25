837ab7959a37efefa65c5f316d61c0be9bbf7a0c
tulasi <tulasipinjala.com>
2019-01-25 10:51:39 -0500

    added properties

--- src/main/java/virtualpet/VirtualPet.java
+++ src/main/java/virtualpet/VirtualPet.java
@@ -7,6 +7,7 @@ public class VirtualPet {
 	private int boredom = 100;
 	private int health = 100;
 	private int dirty = 100;
+	private int thirst = 100;
 	
 	private boolean organic = true;
 	
@@ -14,7 +15,7 @@ public class VirtualPet {
 	private int prevBoredom = 100;
 	private int prevHealth = 100;
 	private int prevDirty = 100;
-
+	private int prevThirst = 100;
 	private String name;
 
 	// Constructor
@@ -29,7 +30,9 @@ public class VirtualPet {
 		hunger -= time;
 		boredom -= time;
 		health -= time;
-		if (hunger < 0 || boredom < 0 || health < 0) {
+		thirst -= time;
+		dirty -= time;
+		if (hunger < 0 || boredom < 0 || health < 0 || thirst < 0|| dirty < 0) {
 			System.out.println("Your pet has fainted");
 			System.exit(0);
 		}
@@ -50,12 +53,18 @@ public class VirtualPet {
 		updatePrevProperties();
 		dirty += 50;
 	}
+	public void water() { // Increase distance from 0 dirty
+		updatePrevProperties();
+		thirst += 50;
+	}
+	
 	
 	private void updatePrevProperties() {
 		prevHunger = hunger;
 		prevBoredom = boredom;
 		prevHealth = health;
 		prevDirty = dirty;
+		prevThirst = thirst;
 	}
 	
 	//Accessor method
@@ -80,11 +89,15 @@ public class VirtualPet {
 	public int getDirty() {
 		return dirty;
 	}
+	public int getThirst() {
+		return thirst;
+	}
 	public void getStatus() {
 		System.out.println(name + "\n Hunger is at " + hunger
 				+ "\n Boredom is at " + boredom
 				+ "\n Health is at " + health
-				+ "\n Dirty is at " + dirty);
+				+ "\n Dirty is at " + dirty
+				+ "\n Thirst is at " + thirst);
 	}
 
 //	public void getStatusChange() {
@@ -106,7 +119,9 @@ public class VirtualPet {
 			+ "\n Health \t" + prevHealth + "\t" 
 			+ "=>" + "\t"+ health
 			+ "\n Dirty \t \t" + prevDirty + "\t" 
-			+ "=>" + "\t"+ dirty);
+			+ "=>" + "\t"+ dirty
+			+ "\n Thirst \t \t" + prevThirst + "\t" 
+			+ "=>" + "\t"+ thirst);
 }
 	
 	//special methods
