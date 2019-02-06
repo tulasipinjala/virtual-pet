0b28d2db1dbdb7b47a1cde212f33a433792056c6
tulasi <tulasipinjala.com>
2019-02-06 12:56:29 -0500

    added few more subclasses

---
+++ src/main/java/virtualpet/organic/Cat.java
@@ -0,0 +1,9 @@
+package virtualpet.organic;
+
+public class Cat extends Organic {
+	public Cat(String name) {
+		super(name);
+		super.setSound("Cat");
+	}
+
+}
---
+++ src/main/java/virtualpet/organic/Pig.java
@@ -0,0 +1,9 @@
+package virtualpet.organic;
+
+public class Pig extends Organic {
+	public Pig(String name) {
+		super(name);
+		super.setSound("Pig");
+	}
+
+}
